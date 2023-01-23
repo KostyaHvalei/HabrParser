57d5eb479e050b45645bfbdd1462542f576c0850
Kosty <kostya_khvalei@outlook.com>
2023-01-23 18:30:36 +0300

    Add Article Model

---
+++ HabrParser/Models/Article.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HabrParser.Models;
+
+public class Article
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    
+    public string Creator { get; set; }
+
+    public string Title { get; set; }
+
+    public string Content { get; set; }
+
+    public DateTime PublishedAt { get; set; }
+
+    public string Link { get; set; }
+
+    public string? ImageLink { get; set; }
+}
