383e3a6b06280ecc5473766f8dbb18e036f18264
Kosty <kostya_khvalei@outlook.com>
2023-01-24 16:30:03 +0300

    Add ApplicationContext

---
+++ HabrParser/Data/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using HabrParser.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HabrParser.Data;
+
+public class ApplicationContext : DbContext
+{
+    public ApplicationContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+    
+    public DbSet<Article> Articles { get; set; }
+}
