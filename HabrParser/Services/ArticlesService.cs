8d2e8172aa6576717a8e8f9b42bd565560fa8d70
Kosty <kostya_khvalei@outlook.com>
2023-01-25 12:47:42 +0300

    Add ArticlesService

---
+++ HabrParser/Services/ArticlesService.cs
@@ -0,0 +1,25 @@
+﻿using HabrParser.Contracts;
+
+namespace HabrParser.Services;
+
+public class ArticlesService : IArticlesService
+{
+    private readonly IArticleRepository _articleRepository;
+    private readonly IFeedService _feedService;
+    private readonly IFeedParsingService _feedParsingService;
+
+    public ArticlesService(
+        IArticleRepository articleRepository,
+        IFeedService feedService,
+        IFeedParsingService feedParsingService)
+    {
+        _articleRepository = articleRepository;
+        _feedService = feedService;
+        _feedParsingService = feedParsingService;
+    }
+
+    public Task<int> LoadNewArticlesAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
