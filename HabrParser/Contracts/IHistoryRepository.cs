cb8e0139b516b597d67c348d5d81c1bcb2431370
Kosty <kostya_khvalei@outlook.com>
2023-01-29 22:07:19 +0300

    Add HistoryRepository

---
+++ HabrParser/Contracts/IHistoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using HabrParser.Models;
+
+namespace HabrParser.Contracts;
+
+public interface IHistoryRepository
+{
+    public Task<IEnumerable<LoadInfo>> GetFullHistory();
+    public Task Add(LoadInfo loadInfo);
+}
---
+++ HabrParser/Repository/HistoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using HabrParser.Contracts;
+using HabrParser.Data;
+using HabrParser.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HabrParser.Repository;
+
+public class HistoryRepository : IHistoryRepository
+{
+    private readonly ApplicationContext _context;
+
+    public HistoryRepository(ApplicationContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<IEnumerable<LoadInfo>> GetFullHistory()
+    {
+        return await _context.History
+            .OrderByDescending(loadInfo => loadInfo.LoadedAt)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task Add(LoadInfo loadInfo)
+    {
+        await _context.History.AddAsync(loadInfo);
+        await _context.SaveChangesAsync();
+    }
+}
