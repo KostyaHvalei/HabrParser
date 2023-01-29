f48e59e1d2fa017a4cf9f8e2449ca7d36a007f15
Kosty <kostya_khvalei@outlook.com>
2023-01-29 14:23:35 +0300

    Implement LoadNewArticlesAsync in ArticlesService

--- HabrParser/Services/ArticlesService.cs
+++ HabrParser/Services/ArticlesService.cs
@@ -1,4 +1,5 @@
 ﻿using HabrParser.Contracts;
+using HabrParser.Models;
 
 namespace HabrParser.Services;
 
@@ -7,19 +8,43 @@ public class ArticlesService : IArticlesService
     private readonly IArticleRepository _articleRepository;
     private readonly IFeedService _feedService;
     private readonly IFeedParsingService _feedParsingService;
+    private readonly IConfiguration _configuration;
 
     public ArticlesService(
         IArticleRepository articleRepository,
         IFeedService feedService,
-        IFeedParsingService feedParsingService)
+        IFeedParsingService feedParsingService,
+        IConfiguration configuration)
     {
         _articleRepository = articleRepository;
         _feedService = feedService;
         _feedParsingService = feedParsingService;
+        _configuration = configuration;
     }
 
-    public Task<int> LoadNewArticlesAsync()
+    public async Task<int> LoadNewArticlesAsync()
     {
-        throw new NotImplementedException();
+        var lastArticle = await _articleRepository.GetLastAsync();
+        var newArticles = new List<Article>();
+        var maxPageNumber = Int32.Parse(_configuration.GetSection("MaxHabrFeedPageNumber").Value);
+        
+        for (int pageNumber = 1; pageNumber <= maxPageNumber; pageNumber++)
+        {
+            var pageContent = await _feedService.LoadPageAsync(pageNumber);
+            var pageArticles = await _feedParsingService.ParseRSSPageAsync(pageContent);
+
+            int lastArticleIdx = pageArticles.IndexOf(lastArticle);
+            
+            if(lastArticleIdx == -1)
+                newArticles.AddRange(pageArticles);
+            else
+            {
+                newArticles.AddRange(pageArticles.GetRange(0, lastArticleIdx));
+                break;
+            }
+        }
+
+        newArticles.Reverse();
+        return await _articleRepository.AddArticlesAsync(newArticles);
     }
 }
