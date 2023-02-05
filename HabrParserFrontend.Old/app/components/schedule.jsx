9776a7766dd44ba4df735b3900d7c8432122c1e3
Kosty <kostya_khvalei@outlook.com>
2023-02-05 17:01:35 +0300

    Add old frontend

---
+++ HabrParserFrontend.Old/app/components/schedule.jsx
@@ -0,0 +1,73 @@
+﻿import React from "react";
+import ScheduleForm from "./scheduleform.jsx";
+import ClearScheduleButton from "./clearschedulebutton.jsx";
+
+export default class Schedule extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            schedule: "",
+            isLoaded: false,
+        };
+
+        this.getSchedule = this.getSchedule.bind(this);
+    }
+    async componentDidMount() {
+        await this.getSchedule();
+    }
+
+    async getSchedule() {
+        await fetch("https://localhost:44339/api/schedule")
+            .then((response) => response.text())
+            .then((data) => {
+                this.setState({ schedule: data, isLoaded: true });
+            });
+    }
+
+    async rerenderCallback() {
+        await this.getSchedule();
+    }
+
+    render() {
+        const isLoaded = this.state.isLoaded;
+
+        if (isLoaded) {
+            if (this.state.schedule.length >= 15) {
+                return (
+                    <div
+                        id="layout-content"
+                        className="layout-content-wrapper col-md-6"
+                    >
+                        <p>There is no schedule. You can make it:</p>
+                        <ScheduleForm
+                            rerenderParentCallback={this.getSchedule}
+                        />
+                    </div>
+                );
+            } else {
+                return (
+                    <div
+                        id="layout-content"
+                        className="layout-content-wrapper col-md-6"
+                    >
+                        <p>Current Cron Schedule: {this.state.schedule}</p>
+                        <p>You can update it: </p>
+                        <ScheduleForm
+                            rerenderParentCallback={this.getSchedule}
+                        />
+                        <p>Or delete it:</p>
+                        <ClearScheduleButton
+                            rerenderParentCallback={this.getSchedule}
+                        />
+                    </div>
+                );
+            }
+        } else {
+            return (
+                <h1>
+                    <div className="col">Loading...</div>
+                </h1>
+            );
+        }
+    }
+}
---
+++ HabrParserFrontend.Old/app/components/scheduleform.jsx
@@ -0,0 +1,49 @@
+﻿import React from "react";
+
+export default class ScheduleForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { schedule: "" };
+
+        this.onChange = this.onChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    onChange(e) {
+        let value = e.target.value;
+        this.setState({ schedule: value });
+    }
+
+    async send() {
+        let cronschedule = { cronschedule: this.state.schedule };
+        const response = await fetch("https://localhost:44339/api/schedule", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(cronschedule),
+        }).then((data) => console.log(data));
+    }
+
+    async handleSubmit(e) {
+        await this.send();
+        await this.props.rerenderParentCallback();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                    <label>Schedule: </label>
+                    <input
+                        type="text"
+                        value={this.state.schedule}
+                        onChange={this.onChange}
+                    />
+                </div>
+                <input type="submit" value="Send" className="btn btn-primary" />
+            </form>
+        );
+    }
+}
